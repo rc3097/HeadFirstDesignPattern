b8f59a28d212dd95e6abab8e730550a64a099fb3
rc3097 <rc3097@nyu.edu>
2016-06-17 14:12:16 -0400

    CH10

---
+++ CH10/src/newgumball/GumballMachineTestDrive.java
@@ -0,0 +1,38 @@
+package newgumball;
+
+public class GumballMachineTestDrive {
+	public static void main(String[] args) {
+		GumballMachine gumballMachine = new GumballMachine(5);
+		
+		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		
+		System.out.println(gumballMachine);
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		
+		gumballMachine.refill(5);
+		gumballMachine.insertQuarter();
+		gumballMachine.turnCrank();
+		
+		System.out.println(gumballMachine);
+	}
+}
